7af426e522a56824e6a1adb5e4e6d1cf7dd9e54a
Steins-trait <abbasamirza44@gmail.com>
2025-05-03 03:43:26 -0400

    optimized for my iphone

--- js/scroll-cue.js
+++ js/scroll-cue.js
@@ -2,12 +2,12 @@ const scrollCue = document.querySelector('.scroll-cue');
 
 // Position it at the bottom of the initial viewport
 window.addEventListener('load', () => {
-  const initialOffset = window.innerHeight * 0.8; // 80% of viewport height
+  const initialOffset = window.innerHeight * 0.85; // 85% of viewport height
   scrollCue.style.top = `${initialOffset}px`;
 });
 
 window.addEventListener('scroll', () => {
-  if (window.scrollY > 80) {
+  if (window.scrollY > 8) {
     scrollCue.classList.add('scroll-cue--hidden');
   } else {
     scrollCue.classList.remove('scroll-cue--hidden');
