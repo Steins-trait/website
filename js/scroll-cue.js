7b7e3fc14f05b1fb7460f81a4744495b02ad07f7
Steins-trait <abbasamirza44@gmail.com>
2025-05-03 03:35:25 -0400

    Update scroll-cue.js

--- js/scroll-cue.js
+++ js/scroll-cue.js
@@ -2,7 +2,7 @@ const scrollCue = document.querySelector('.scroll-cue');
 
 // Position it at the bottom of the initial viewport
 window.addEventListener('load', () => {
-  const initialOffset = window.innerHeight * 0.8; // 90% of viewport height
+  const initialOffset = window.innerHeight * 0.8; // 80% of viewport height
   scrollCue.style.top = `${initialOffset}px`;
 });
 
